d94d56812b14f9ad67b11c14d0a9365ec8384d4d
Abhishek R Nair <abhiattipra@gmail.com>
2024-02-22 22:36:09 +0530

    Update origin in CorsOption

--- backend/app.ts
+++ backend/app.ts
@@ -28,7 +28,7 @@ import { frontendPort, getBackendPort } from "../src/utils/portUtils";
 require("dotenv").config();
 
 const corsOption = {
-  origin: `http://localhost:${frontendPort}`,
+  origin: ['http://localhost:${frontendPort}', 'https://cypress-app.vercel.app'],
   credentials: true,
 };
 
